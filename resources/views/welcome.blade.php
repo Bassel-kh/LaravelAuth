9b25ef9af3efbecbc450a2ec4c224a3a18d39ec8
Bassel-kh <bassel.kh.svu@gmail.com>
2020-06-15 11:19:28 -0700

    Master 2020

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Bassel Khalil
+                    Bassel Khalil Master 2020
                 </div>
 
                 <div class="links">
