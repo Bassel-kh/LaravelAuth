6c71040caf1df213ddfb42642c62c9e350ebda2c
Bassel-kh <bassel.kh.svu@gmail.com>
2020-06-15 07:52:22 -0700

    EP14 Adding foreign key constraints

--- database/migrations/2020_06_15_045912_create_role_user_table.php
+++ database/migrations/2020_06_15_045912_create_role_user_table.php
@@ -14,9 +14,12 @@ class CreateRoleUserTable extends Migration
     public function up()
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->id();
-            $table->integer('role_id')->unsigned();
-            $table->integer('user_id')->unsigned();
+            //$table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('role_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            // $table->integer('role_id')->unsigned();
+            // $table->integer('user_id')->unsigned();
 
             $table->timestamps();
         });
